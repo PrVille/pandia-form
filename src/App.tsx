d629af11398de14a2f7ec1b8f667bc8a5698d0f6
PrVille <ville.prami@me.com>
2024-03-05 12:52:41 +0200

    Global styles, label styles, input styles, select styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,84 @@
 import { useState, FormEvent } from "react"
-import styled from "styled-components"
+import styled, { createGlobalStyle } from "styled-components"
 
-const StyledApp = styled.main``
+import chevronDown from "./assets/chevron-down.svg"
+
+const GlobalStyles = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    margin: 0 auto;
+    font-family: Arial, Helvetica, sans-serif;
+    color: dimgray;
+  }
+`
+
+const StyledApp = styled.main`
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const StyledTitle = styled.h4`
+  margin-bottom: 20px;
+`
 
 const StyledForm = styled.form`
   display: flex;
   flex-direction: column;
-  width: 300px;
+  padding: 20px;
+  border-width: 1px;
+  border-style: solid;
+  border-radius: 8px;
+  max-width: 360px;
   margin: auto;
 `
 
 const StyledLabel = styled.label`
-  margin-bottom: 10px;
+  font-size: 0.75em;
+  font-weight: 700;
+  margin-bottom: 5px;
 `
 
 const StyledInput = styled.input`
-  margin-bottom: 20px;
+  border-radius: 4px;
+  border-style: solid;
+  border-width: 1px;
+  padding: 15px 10px;
+  margin-bottom: 15px;
 `
 
 const StyledSelect = styled.select`
-  margin-bottom: 20px;
+  border-radius: 4px;
+  border-style: solid;
+  border-width: 1px;
+  padding: 15px 10px;
+  margin-bottom: 15px;
+  appearance: none;
+  -webkit-appearance: none;
+  background: url(${chevronDown});
+  background-repeat: no-repeat;
+  background-position-x: calc(100% - 4px);
+  background-position-y: center;
+  background-size: 24px;
 `
 
 const StyledButton = styled.button`
-  background-color: green;
+  background-color: #128559;
   color: white;
   padding: 10px;
   border: none;
   cursor: pointer;
 `
+
+const songs = ["Song 1", "Song 2", "Song 3"]
 
 interface FormState {
   name: string
@@ -51,56 +101,67 @@ const App = () => {
   }
 
   return (
-    <StyledApp>
-      <StyledForm onSubmit={handleSubmit}>
-        <StyledLabel>Nimi tai nimimerkki*</StyledLabel>
-        <StyledInput
-          type="text"
-          value={formState.name}
-          onChange={({ target }) =>
-            setFormState({ ...formState, name: target.value })
-          }
-          required
-        />
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledTitle>Ilmoittautumislomake</StyledTitle>
 
-        <StyledLabel>Biisi*</StyledLabel>
-        <StyledSelect
-          value={formState.song}
-          onChange={({ target }) =>
-            setFormState({ ...formState, song: target.value })
-          }
-          required
-        >
-          <option value="">Valitse alta</option>
-          <option value="biisi">biisi</option>
-        </StyledSelect>
+          <StyledLabel>Nimi tai nimimerkki*</StyledLabel>
+          <StyledInput
+            type="text"
+            value={formState.name}
+            onChange={({ target }) =>
+              setFormState({ ...formState, name: target.value })
+            }
+            required
+          />
 
-        <StyledLabel>Sävellaji*</StyledLabel>
-        <StyledInput
-          type="range"
-          min={-2}
-          max={2}
-          value={formState.key}
-          onChange={({ target }) =>
-            setFormState({ ...formState, key: Number(target.value) })
-          }
-          required
-        />
+          <StyledLabel>Biisi*</StyledLabel>
+          <StyledSelect
+            value={formState.song}
+            onChange={({ target }) =>
+              setFormState({ ...formState, song: target.value })
+            }
+            required
+          >
+            <option value="" hidden>
+              Valitse alta
+            </option>
+            {songs.map((song, index) => (
+              <option key={index} value={song}>
+                {song}
+              </option>
+            ))}
+          </StyledSelect>
 
-        <StyledLabel>
-          Sallin tietojeni tallennuksen karaokejärjestelmään
-        </StyledLabel>
-        <StyledInput
-          type="checkbox"
-          checked={formState.consent}
-          onChange={({ target }) =>
-            setFormState({ ...formState, consent: target.checked })
-          }
-        />
+          <StyledLabel>Sävellaji*</StyledLabel>
+          <StyledInput
+            type="range"
+            min={-2}
+            max={2}
+            value={formState.key}
+            onChange={({ target }) =>
+              setFormState({ ...formState, key: Number(target.value) })
+            }
+            required
+          />
 
-        <StyledButton type="submit">Ilmoittaudu</StyledButton>
-      </StyledForm>
-    </StyledApp>
+          <StyledLabel>
+            Sallin tietojeni tallennuksen karaokejärjestelmään
+          </StyledLabel>
+          <StyledInput
+            type="checkbox"
+            checked={formState.consent}
+            onChange={({ target }) =>
+              setFormState({ ...formState, consent: target.checked })
+            }
+          />
+
+          <StyledButton type="submit">Ilmoittaudu</StyledButton>
+        </StyledForm>
+      </StyledApp>
+    </>
   )
 }
 
